d0e8f1c6d989923bf3e8d64e81b45f5d2e7fd660
Aditya Panchal <adpanchal15@gmail.com>
2021-01-22 10:19:39 +0530

    practice done of exam leave day(2021-01-19) today(2021-01-22)

---
+++ Practice/2021-01-22/sort.js
@@ -0,0 +1,25 @@
+const student=[
+    {name:'Aditya',yearOfBirth:1999},
+    {name:'Dinesh',yearOfBirth:1995},
+    {name:'Nikhil',yearOfBirth:2000},
+    {name:'Harshil',yearOfBirth:1998}
+]
+
+console.log(student.sort(sortByYear));
+
+function sortByYear(a,b){
+    const yearOfBirth1=a.yearOfBirth;
+    const yearOfBirth2=b.yearOfBirth;
+    // console.log(yearOfBirth1);
+    // console.log(yearOfBirth2);
+
+    let c=0;
+
+    if(yearOfBirth1>yearOfBirth2){
+        c=1;
+    }else{
+        c=-1;
+    }
+    console.log(c);
+    return c;
+}
