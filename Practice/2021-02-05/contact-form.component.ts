9943da517ec9dbf0d98064882cdf802811c41d0c
Aditya Panchal <adpanchal15@gmail.com>
2021-02-05 19:07:37 +0530

    (2021-02-05) task done

---
+++ Practice/2021-02-05/contact-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.css']
+})
+export class ContactFormComponent {
+    log(x){ console.log(x) };     //for print property of ngModel variable
+
+    submit(f){                    //for print property of ngForm
+      console.log(f);  
+    }
+    
+}
