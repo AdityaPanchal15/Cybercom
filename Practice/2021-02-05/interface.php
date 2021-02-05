e0b1a3a1b8d4d292857dd11d6dd114b63bbf5a0f
Aditya Panchal <adpanchal15@gmail.com>
2021-02-05 19:05:41 +0530

    (2021-02-05) OOP practice task done

---
+++ Practice/2021-02-05/interface.php
@@ -0,0 +1,45 @@
+<?php
+interface Car { 
+    public function setModel($name);
+    
+    public function getModel();
+  }
+
+interface Vehicle {
+    public function setHasWheels($bool); 
+   
+    public function getHasWheels();
+  }
+
+  class miniCar implements Car, Vehicle {
+    private $model; 
+    private $hasWheels; 
+    
+    public function setModel($name)
+    { 
+      $this -> model = $name; 
+    }
+    
+    public function getModel()
+    {
+      return $this -> model; 
+    }
+    
+    public function setHasWheels($bool)
+    { 
+      $this -> hasWheels = $bool; 
+    }
+    
+    public function getHasWheels()
+    {
+    return ($this -> hasWheels)? "has wheels" : "no wheels";
+    }
+  }
+
+  $myCar=new miniCar;
+  $myCar->setModel('BMW');
+  echo $myCar->getModel()."<br/>";
+  echo $myCar->getHasWheels();
+  
+  
+?>
---
+++ Practice/2021-02-05/preventoverride.php
@@ -0,0 +1,23 @@
+<?php
+class Car {
+    final public function hello()
+    {
+      return "beep";
+    }
+  }
+   
+  //The child class has hello method that tries to override the hello method in the parent
+  class SportsCar extends Car {
+    public function hello()
+    {
+      return "Hallo";
+    }
+  }
+    
+   
+  //Create a new object
+  $sportsCar1 = new SportsCar();
+    
+  //Get the result of the hello method
+  echo $sportsCar1 -> hello();
+?>
