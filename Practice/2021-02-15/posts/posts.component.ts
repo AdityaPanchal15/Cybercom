82221f6771a713b5dc4d8b0b81fb122323c9ac87
Aditya Panchal <adpanchal15@gmail.com>
2021-02-15 13:04:24 +0530

    (2021-02-15) task done

---
+++ Practice/2021-02-15/posts/posts.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../services/post.service';
+import { AppError } from '../common/validators/app-error';
+import { NotFoundError } from '../common/validators/not-founderror';
+import { BadInput } from '../common/validators/bad-input';
+
+@Component({
+  selector: 'posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent implements OnInit {
+  posts:any[];
+
+  constructor(private service:PostService) {  
+   }
+
+   ngOnInit(){
+    this.service.getPost()
+    .subscribe(
+      response=>{
+        this.posts=response.json();
+      },
+      error=>{
+        alert('An unexpected error occured.');
+        console.log(error);
+    });
+   }
+   createPost(input:HTMLInputElement){
+      let post={title:input.value}
+      input.value='';
+
+      this.service.createPost(post)
+        .subscribe(
+          response=>{
+            post['id']=response.json().id;
+            this.posts.splice(0,0,post);
+          },
+          (error:AppError)=>{
+            if(error instanceof BadInput){
+              // this.form.setErrors(error.originalError);
+            }
+            else{
+              alert('An unexpected error occured.');
+              console.log(error);
+            }
+            
+        })
+   }
+
+   updatePost(post){
+    this.service.updatePost(post)
+     .subscribe(
+      response=>{
+        console.log(response.json()); 
+      },
+      error=>{
+        alert('An unexpected error occured.');
+        console.log(error);
+    });
+      // this.http.put(this.url,JSON.stringify(post))
+    }
+
+    deletePost(post){
+      this.service.deletePost(345)
+        .subscribe(
+          response=>{
+            let index = this.posts.indexOf(post);
+            this.posts.splice(index,1);
+          },
+          (error:AppError)=>{
+            if(error instanceof NotFoundError){
+              alert('This post has already deleted');
+            }  
+            else{
+              alert('An unexpected error occured.');
+              console.log(error);
+            }   
+        });
+    }
+
+}
---
+++ Practice/2021-02-15/services/post.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/catch'; 
+import { AppError } from '../common/validators/app-error';
+import { NotFoundError } from '../common/validators/not-founderror';
+import { BadInput } from '../common/validators/bad-input';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+  private url='http://jsonplaceholder.typicode.com/posts';
+
+  constructor(private http:Http) { }
+
+  getPost(){
+    return this.http.get(this.url);
+  }
+  
+  createPost(post){
+    return this.http.post(this.url,JSON.stringify(post))
+      .catch((error:Response)=>{
+        if(error.status === 404)
+          return Observable.throw(new BadInput(error.json()));
+
+        return Observable.throw(new AppError(error.json()));
+    });
+  }
+
+  updatePost(post){
+    return this.http.patch(this.url+'/'+post.id ,JSON.stringify({isRead:true}));
+  }
+
+  deletePost(id){
+    return this.http.delete(this.url + '/' + id)
+      .catch((error:Response)=>{
+        if(error.status === 404)
+          return Observable.throw(new NotFoundError());
+
+        return Observable.throw(new AppError(error));
+      });
+  }
+}
