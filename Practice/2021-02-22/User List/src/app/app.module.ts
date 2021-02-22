af40c15c42711227f72fd4121b3b15a6446ab0ab
Aditya Panchal <adpanchal15@gmail.com>
2021-02-22 19:25:14 +0530

    user list task done

---
+++ Practice/2021-02-22/User List/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserListService } from './core/services/users/user-list.service';
+import { HttpClientModule } from '@angular/common/http';
+import { UsersComponent } from './users/users.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [UserListService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Practice/2021-02-22/User List/src/app/users/users.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { UserListService } from '../core/services/users/user-list.service';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+  _title = 'mintop';
+  _userArray = [];
+
+  constructor(private userService:UserListService) { }
+
+  ngOnInit(): void {
+    try {
+      this.userService.getUsers().subscribe((response) => {
+        console.log(response);
+        this._userArray = response["data"];
+      })
+    } catch (error) {
+      throw new Error(error);
+    }
+    
+  }
+
+}
