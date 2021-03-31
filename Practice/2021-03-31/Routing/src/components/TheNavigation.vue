ee92915ad2e73fd82c16ef629980e180e941c638
Aditya Panchal <adpanchal15@gmail.com>
2021-03-31 18:59:32 +0530

    renderFunction and jsx,filter and routing

---
+++ Practice/2021-03-31/Routing/src/components/TheNavigation.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <nav id="nav">
+      <p class="logo">The Vue School Route Guide</p>
+      <ul class="nav-links">
+        <li class="links">
+          <router-link to="/"> Home </router-link>
+        </li>
+        <li
+          v-for="destination in destinations"
+          :key="destination.name"
+          class="links"
+        >
+          <router-link
+            :to="{ name: 'DestinationDetails', params: { id: destination.id } }"
+          >
+            {{ destination.name }}
+          </router-link>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+<script>
+import store from "../store";
+export default {
+  data() {
+    return {
+      destinationId: this.$route.params.id,
+      destinations: store.destinations,
+    };
+  },
+};
+</script>
+<style scoped>
+#nav {
+  display: flex;
+}
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  text-decoration: none;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+.nav-links {
+  display: flex;
+}
+.links {
+  padding-right: 20px;
+  list-style: none;
+}
+.links:hover {
+  text-decoration: underline;
+}
+.logo {
+  font-size: 20px;
+  color: purple;
+  font-weight: bold;
+}
+</style>
