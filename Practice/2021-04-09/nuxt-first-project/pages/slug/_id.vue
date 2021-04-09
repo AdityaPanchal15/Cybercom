69b5ce8769660cf6faa604ec7550dd4d94bee612
Aditya Panchal <adpanchal15@gmail.com>
2021-04-09 19:08:44 +0530

    NuxtJs introduction and sample app

---
+++ Practice/2021-04-09/nuxt-first-project/pages/slug/_id.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <h1>Id: {{this.$route.params.id}}</h1>
+  <h1>Query: {{this.$route.query.message}} {{this.$route.query.message1}}</h1>
+  <p>{{post}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+async asyncData({params,$http,error}) {
+    const id = params.id
+    try {
+      // Using the nuxtjs/http module here exposed via context.app
+      const post = await $http.$get(
+        `https://api.nuxtjs.dev/posts/${id}`
+      )
+      return { post }
+    } catch (e) {
+      error(e) // Show the nuxt error page with the thrown error
+    }
+  },
+  
+}
+</script>
+
+<style>
+
+</style>
