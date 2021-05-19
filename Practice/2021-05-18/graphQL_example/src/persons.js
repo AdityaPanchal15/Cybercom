27dd5b337e33e8087c9202269d4a675594ac59ce
Aditya Panchal <adpanchal15@gmail.com>
2021-05-19 18:58:52 +0530

    (2021-05-19)graphQL crud operation example done

---
+++ Practice/2021-05-18/graphQL_example/src/persons.js
@@ -0,0 +1,10 @@
+module.exports = [
+	{
+		id: 1,
+		name: 'a',
+		age: 18,
+		posts: [{ id: 1, title: 'abc', author: { id: 1, name: 'a', age: 18 } }],
+	},
+	{ id: 2, name: 'b', age: 19, posts: [{ id: 2, title: 'abcd', author: { id: 2, name: 'b', age: 19 } }] },
+	{ id: 3, name: 'c', age: 16, posts: [{ id: 3, title: 'ab', author: { id: 3, name: 'c', age: 16 } }] },
+];
