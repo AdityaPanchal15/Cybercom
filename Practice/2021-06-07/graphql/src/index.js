d7c17abb606bafabfd51293cfc2033d41c093421
Aditya Panchal <adpanchal15@gmail.com>
2021-06-10 19:12:02 +0530

    (2021-06-10) complete graphql connection with product,subproduct

--- Practice/2021-06-07/graphql/src/index.js
+++ Practice/2021-06-07/graphql/src/index.js
@@ -1,57 +1,83 @@
 const { ApolloServer, gql } = require('apollo-server');
-const Products = require('./datasource/products');
+// const Products = require('./datasource/products');
 const policy = require('./datasource/policy');
+const product = require('./datasource/product');
+const { keys } = require('./datasource/product');
+const subCategory = require('./datasource/subCategory');
 
 const typeDefs = gql`
 	type Query {
-		hello: String
-		products: [Product]
+		product(productType:String!): Product
 		policy(url:String): Policy
+		subCategory(categoryType:String!):[subCategory]
 	}
-	type Product {
-		entity_id: String
-		type_id: String
-		sku: String
-		created_at: String
-		updated_at: String
-		description: String
-		short_description: String
-		color: String
-		material_type: String
-		style: String
-		mattress_type: String
-		also_available: String
-		bed_size_measure: String
-		bed_type: String
-		clock_type: String
-		mattress_size: String
-		mattress_position: String
-		mattress_comfort: String
-		mattress_top: String
-		pillow_size: String
-		pillow_fill: String
-		brand: String
-		collection_type: String
-		product_type: String
-		corner_type: String
-		coffee_table_features: String
-		comfort_level: String
-		key_features: String
-		table_top_style: String
-		table_top_shape: String
-		tailoring: String
-		mattress_thickness: String
-		is_suspended: String
-		name: String
-		meta_title: String
-		meta_description: String
-		regular_price_with_tax: Int
-		regular_price_without_tax: Int
-		final_price_with_tax: Int
-		final_price_without_tax: Int
-		is_saleable: Boolean
-		image_url: String
+	type Product{
+		productLinks:[Link]
+		img:String
+		categories:[Category]
 	}
+	type Link {
+		linkTitle:String
+		link:String
+	}
+	type Category {
+		src:String
+		name:String
+	}
+	type subCategory {
+		src:String
+		title:String
+		subTitle:String
+		webId:String
+		price:String
+		originalPrice:String
+		numberOfRating:String
+		rating:Float
+	}
+	# type Product {
+	# 	entity_id: String
+	# 	type_id: String
+	# 	sku: String
+	# 	created_at: String
+	# 	updated_at: String
+	# 	description: String
+	# 	short_description: String
+	# 	color: String
+	# 	material_type: String
+	# 	style: String
+	# 	mattress_type: String
+	# 	also_available: String
+	# 	bed_size_measure: String
+	# 	bed_type: String
+	# 	clock_type: String
+	# 	mattress_size: String
+	# 	mattress_position: String
+	# 	mattress_comfort: String
+	# 	mattress_top: String
+	# 	pillow_size: String
+	# 	pillow_fill: String
+	# 	brand: String
+	# 	collection_type: String
+	# 	product_type: String
+	# 	corner_type: String
+	# 	coffee_table_features: String
+	# 	comfort_level: String
+	# 	key_features: String
+	# 	table_top_style: String
+	# 	table_top_shape: String
+	# 	tailoring: String
+	# 	mattress_thickness: String
+	# 	is_suspended: String
+	# 	name: String
+	# 	meta_title: String
+	# 	meta_description: String
+	# 	regular_price_with_tax: Int
+	# 	regular_price_without_tax: Int
+	# 	final_price_with_tax: Int
+	# 	final_price_without_tax: Int
+	# 	is_saleable: Boolean
+	# 	image_url: String
+	# }
 	type Policy {
 		pageInfo: PageInfo
 		content: Content
@@ -95,15 +121,22 @@ const typeDefs = gql`
 `;
 const resolvers = {
 	Query: {
-		hello: (_, __, { dataSources }) => {
-			dataSources.products.ProductAPI();
-			return 'hello world';
-		},
-		products: (_, __, { dataSources }) => {
-			return dataSources.products.ProductAPI();
-		},
+		// hello: (_, __, { dataSources }) => {
+		// 	dataSources.products.ProductAPI();
+		// 	return 'hello world';
+		// },
+		// products: (_, __, { dataSources }) => {
+		// 	return dataSources.products.ProductAPI();
+		// },
 		policy:(_,{url})=>{
 			const result = policy.find((data)=>data.pageInfo.url===url)
+			return result
+		},
+		product:(_,{productType})=>{
+			return product[productType]
+		},
+		subCategory:(_,{categoryType})=>{
+			const result = subCategory[categoryType]
 			return result
 		}
 	},
@@ -112,9 +145,9 @@ const resolvers = {
 const server = new ApolloServer({
 	typeDefs,
 	resolvers,
-	dataSources: () => {
-		return { products: new Products() };
-	},
+	// dataSources: () => {
+	// 	return { product: new Product() };
+	// },
 });
 
 server.listen().then(({ url }) => {
